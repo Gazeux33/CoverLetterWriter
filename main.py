bf72f94af742c0f8647bc873d2886bad8d3bd3e4
tcastillo-ubuntu <theocastillo@yahoo.com>
2025-01-25 18:04:37 +0100

    update args readme.md

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from src.writer import CoverLetterWriter
+import argparse
 
 
 if __name__ == "__main__":
-    writer = CoverLetterWriter()
-    writer.generate_cover_letter("Google")
+    parser = argparse.ArgumentParser(description="Generate a cover letter for a specified company.")
+    parser.add_argument("-n", "--company_name", type=str, required=True, help="The name of the company to generate the cover letter for.")
+    parser.add_argument("-l", "--language", type=str, required=False, default="fr" ,help="The language of the cover letter to generate.")
+    parser.add_argument("-o", "--offer", type=str, required=False ,help="Path to the job offer to generate the cover letter for.")
+
+    args = parser.parse_args()
+
+
+    writer = CoverLetterWriter(language=args.language, offer_path=args.offer)
+    writer.generate_cover_letter(args.company_name)
